f2e3ae9795717dce766de0d0e9c3b6d0f76b773f
Humaira <humaira.annura@gmail.com>
2024-07-26 19:32:01 +0800

    chore: hello world,.. again

--- hello_test.go
+++ hello_test.go
@@ -7,10 +7,21 @@ import "testing"
 // testing new branch
 
 func TestHello(t *testing.T) {
-	got := Hello("Mai")
-	want := "Hello, Mai"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Mai")
+		want := "Hello, Mai"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
