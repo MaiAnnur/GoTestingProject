5453b202414ce64d19d7d7e152fecb9c5b363d39
Humaira <humaira.annura@gmail.com>
2024-08-05 10:22:55 +0800

    chore:refactor, added +=

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,10 +2,12 @@ package iteration
 
 import "fmt"
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 		//keluarkan value dlu
 		fmt.Printf("i = %d, repeated: %q\n", i, repeated)
 		//i++ run last skali
