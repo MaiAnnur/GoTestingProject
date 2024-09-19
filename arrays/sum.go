544198e214188e7ae303a1d0d70440b4b419e3ed
Humaira <humaira.annura@gmail.com>
2024-09-19 12:24:54 +0800

    chore: fix code to make it pass, using if else

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,12 +1,22 @@
 package arrays
 
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
 	//sum := 0
 	// 	for i := 0; i < 5; i++ {
 	// 		sum += numbers[i]
