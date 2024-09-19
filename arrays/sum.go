a25e872bf9d95857fbdb28008cbdeb2d8f3593cb
Humaira <humaira.annura@gmail.com>
2024-09-19 12:07:44 +0800

    chore: refactor with make, len

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,14 +1,20 @@
 package arrays
 
-func Sum(numbers []int) int {
-	sum := 0
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	//sum := 0
 	// 	for i := 0; i < 5; i++ {
 	// 		sum += numbers[i]
 	// 	}
 	// 	return sum
 	// }
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+	//for _, number := range numbers {
+	//	sum += number
+	//}
+	return sums
 }
