955fe3ec6c4a8031a7c5a1c90917fab95cf03fff
Humaira <humaira.annura@gmail.com>
2024-09-24 11:11:19 +0800

    fix: error on func TestSumAllTails, making new func Sum to call sum

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,30 +1,23 @@
 package arrays
 
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
 func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
 		if len(numbers) == 0 {
 			sums = append(sums, 0)
-
 		} else {
 			tail := numbers[1:]
 			sums = append(sums, Sum(tail))
 		}
 	}
-	// lengthOfNumbers := len(numbersToSum)
-	// sums := make([]int, lengthOfNumbers)
 
-	// for i, numbers := range numbersToSum {
-	// 	sums[i] = Sum(numbers)
-	// }
-	//sum := 0
-	// 	for i := 0; i < 5; i++ {
-	// 		sum += numbers[i]
-	// 	}
-	// 	return sum
-	// }
-	//for _, number := range numbers {
-	//	sum += number
-	//}
 	return sums
 }
